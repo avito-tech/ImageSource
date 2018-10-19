2313a5b5146d3bd5b1de67e13d2d9ba5211eb4e5
Sergey Sayfulin <sosayfulin@avito.ru>
2018-10-19 21:27:57 +0300

    SEL-2721: Make image request option constant

--- ImageSource/Core/ImageRequestOptions.swift
+++ ImageSource/Core/ImageRequestOptions.swift
@@ -2,14 +2,16 @@ public struct ImageRequestOptions {
     
     public var size: ImageSizeOption = .fullResolution
     public var deliveryMode: ImageDeliveryMode = .best
-    public var needsMetadata: Bool = false
+    public let needsMetadata: Bool
     
     /// Called on main thread when image download starts
     public var onDownloadStart: ((ImageRequestId) -> ())?
     /// Called on main thread when image download finishes
     public var onDownloadFinish: ((ImageRequestId) -> ())?
     
-    public init() {}
+    public init() {
+        self.needsMetadata = false
+    }
     
     public init(size: ImageSizeOption, deliveryMode: ImageDeliveryMode, needsMetadata: Bool = false) {
         self.size = size
