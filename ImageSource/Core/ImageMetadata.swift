0e99e19a5f8ba4dcc7c875a8a7b6b6c7f8bf8b13
Sergey Sayfulin <sosayfulin@avito.ru>
2018-10-22 12:59:42 +0300

    SEL-2721: Add serializable check

--- ImageSource/Core/ImageMetadata.swift
+++ ImageSource/Core/ImageMetadata.swift
@@ -7,7 +7,7 @@ public struct ImageMetadata {
     }
     
     public init(_ metadata: [String: Any]?) {
-        self.metadata = metadata ?? [:]
+        self.metadata = ImageMetadata.filteredAsJSONSerializable(metadata ?? [:])
     }
     
     public init(_ metadata: [NSObject: AnyObject]?) {
@@ -26,6 +26,18 @@ public struct ImageMetadata {
             if let nested = value as? [NSObject: AnyObject] {
                 result[key] = ImageMetadata.convertObjcMetadata(nested) as Any
             } else if JSONSerialization.isValidJSONObject([value]) { // Meta must contain only JSON serializable items
+                result[key] = value as Any
+            }
+        }
+        return result
+    }
+    
+    private static func filteredAsJSONSerializable(_ metadata: [String: Any]) -> [String: Any] {
+        var result = [String: Any]()
+        for (key, value) in metadata {
+            if let nested = value as? [String: Any] {
+                result[key] = ImageMetadata.filteredAsJSONSerializable(nested) as Any
+            } else if JSONSerialization.isValidJSONObject([value]) {
                 result[key] = value as Any
             }
         }
